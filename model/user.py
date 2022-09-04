2109e335ba422e60395d403206d951e7962cd226
Aleks22f <tsvigun16@mail.ru>
2022-09-04 23:52:48 +1000

    добавил модель пользователя

---
+++ model/user.py
@@ -0,0 +1,10 @@
+class user():
+    __surname: str
+    __name: str
+    __patronymic: str
+    __dateofbirth: str
+    def __init__(self, surname, name, patronymic, dataofbith):
+        self.__surname=surname
+        self.__name=name
+        self.__patronymic=patronymic
+        self.__dateofbirth=dataofbith
