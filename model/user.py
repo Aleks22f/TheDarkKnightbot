0cf7075c59b8a9950359233d09b65fe16a80b572
Aleks22f <tsvigun16@mail.ru>
2022-09-05 00:17:48 +1000

    изменино date_of_birth

--- model/user.py
+++ model/user.py
@@ -1,10 +1,11 @@
-class user():
+class User:
     __surname: str
     __name: str
     __patronymic: str
-    __dateofbirth: str
-    def __init__(self, surname, name, patronymic, dataofbith):
-        self.__surname=surname
-        self.__name=name
-        self.__patronymic=patronymic
-        self.__dateofbirth=dataofbith
+    __date_of_birth: str
+
+    def __init__(self, surname, name, patronymic, date_of_birth):
+        self.__surname = surname
+        self.__name = name
+        self.__patronymic = patronymic
+        self.__date_of_birth = date_of_birth
