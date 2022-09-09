ebb79b35c9c5e1076ff0d1b04e7e59e69e4c61a8
Максим Шевченков <m.shevchenkov@white-soft.ru>
2022-09-09 15:02:45 +0300

    test

--- controller/dkbot_controller.py
+++ controller/dkbot_controller.py
@@ -1,11 +1,22 @@
 import telebot
+from utils import read_config
+from telebot import types
 
-token = "5486480610:AAG__Eowe3JnF-QJFXSk_4vltJxWVf6IVC0"
+token = read_config(read_config("config/config.json")["bot_api_key"])
 
 bot = telebot.TeleBot(token)
 
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет!')
+    bot.send_message(message.chat.id, 'Привет! Прокинь ФИО')
+
+
+@bot.message_handler(commands=['hui'])
+def fio(message):
+    markup_inline = types.InlineKeyboardMarkup()
+    pohui = types.InlineKeyboardButton(text='ПОЛОЖИТЬ ХУЙ', callback_data='ВЫ ТОЛЬКО ЧТО ПОЛОЖИЛИ НА ВСЕ ХУЙ))')
+    markup_inline.add(pohui)
+
 
 bot.polling()
